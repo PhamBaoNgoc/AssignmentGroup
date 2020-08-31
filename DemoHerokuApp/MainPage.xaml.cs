d4661473a2eb83b13cee8342a26dc95c121e04c0
PhamBaoNgoc <59905079+PhamBaoNgoc@users.noreply.github.com>
2020-08-31 12:06:36 +0700

    Add project files.

---
+++ DemoHerokuApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DemoHerokuApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using static DemoHerokuApp.Models.ContentNetwork;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoHerokuApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private async void AllPage(object sender, RoutedEventArgs e)
+        {
+            Root myContentNetwork = await ContentNetwork.GetContentNetwork();
+            string image = String.Format(myContentNetwork.image);
+            Image.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
+            TemplateTextBlock.Text = myContentNetwork.title;
+            ContentDescriptionTextBlock.Text = Convert.ToString(myContentNetwork.content.description);
+            LocationTextBlock.Text = myContentNetwork.date;
+        }
+    }
+}
---
+++ DemoHerokuApp/Models/ContentNetWork.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoHerokuApp.Models
+{
+     public class ContentNetwork
+    {
+        public async static Task<Root> GetContentNetwork()
+        {
+
+            var http = new HttpClient();
+            var url = string.Format("http://api-demo-anhth.herokuapp.com/data.json");
+
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractJsonSerializer(typeof(Root));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (Root)serializer.ReadObject(ms);
+            return data;
+        }
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Content
+        {
+           
+            public string description { get; set; }
+            
+            public string url { get; set; }
+        }
+
+        public class Root
+        {
+            
+            public int id { get; set; }
+            
+            public string date { get; set; }
+            
+            public string title { get; set; }
+            
+            public string image { get; set; }
+            
+            public Content content { get; set; }
+        }
+    }
+}
